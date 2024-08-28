fc677d5e9d07fbb216dde53b58b656cf4a670f69
Zechariah <zechariahadebayo42@gmail.com>
2024-08-28 23:52:56 +0100

    feat: additional routes and layout formatting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,19 @@ import SignUp from "./auths/SignUp";
 
 
 export default function App(){
-    return <div className="w-screen h-screen flex-col justify-start items-center bg-green-400">
+    return <div className="w-screen h-screen flex-col justify-start items-center">
             <Router>
                 <Routes>
                     <Route path="/">
                         <Route index element={<>This is the root path</>}/>
                         <Route path="signup/" element={<SignUp/>}/>
                         <Route path="login/" element={<Login/>}/>
+                        <Route path="learn-more/" element={<>This is the learn more path</>}/>
+                        <Route path="about/" element={<>About page</>}/>
+                        <Route path="services/" element={<>Service page</>}/>
+                        <Route path="contact/" element={<>Contact page</>}/>
+                        <Route path="*" element={<Login/>} />
+                        <Route path="*/*" element={<Login/>} />
                     </Route>
                 </Routes>
             </Router>
